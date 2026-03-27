7bcfba6dae6517cc7b57b48f368310e893078f18
UBSITEscuadraSK <20243641@.ubaguio.edu>
2026-03-27 11:25:47 +0800

    Generating of Components

---
+++ TaskFlow/src/app-component/task-details-component/task-details-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-details-component',
+  imports: [],
+  templateUrl: './task-details-component.html',
+  styleUrl: './task-details-component.css',
+})
+export class TaskDetailsComponent {}
